133ee6c8ee8bdaaa9ce6bf75305e583af6a9619f
unknown <aristakesyan_2001@mail.ru>
2024-05-18 21:33:24 +0400

    adding registerModels function

--- scr/database/index.js
+++ scr/database/index.js
@@ -1,63 +1,60 @@
 import cls from 'cls-hooked';
 import { Sequelize } from 'sequelize';
+import { registerModels } from '../modules';
 
 export default class Database {
-    constructor(envirement, dbConfig){
-        this.envirement = envirement;
-        this.dbConfig = dbConfig;
-        this.isTestEnvirement = this.envirement === 'test';
+    constructor(environment, dbConfig) {
+      this.environment = environment;
+      this.dbConfig = dbConfig;
+      this.isTestEnvironment = this.environment === 'test';
     }
-
-    async connect(){
-
-        // Set up transaction namespace
-        const namespace = cls.createNamespace('transections-namespace');
-        Sequelize.useCLS(namespace);
-
-        //Create the connection
-        const{username, password, host, port, database, dialect} =
-        this.dbConfig[this.envirement];
-        this.connect = new Sequelize({
-            username,
-            password,
-            host,
-            port,
-            database,
-            dialect,
-            logging: this.isTestEnvirement ? false:console.log,
-        })
-
-        //Check if we connected successfully
-
-        await this.connect.authenticate({
-            logging:false
-        });
-        
-        if(!this.isTestEnvirement){
-            console.log('Connection has ben established successfully');
-        }
-
-        //Register the moduls
-        //registermoduls(this.connect)
-
-        //sync the modules
-
-        await this.sync();
-
+  
+    async connect() {
+      // Set up the namespace for transactions
+      const namespace = cls.createNamespace('transactions-namespace');
+      Sequelize.useCLS(namespace);
+  
+      // Create the connection
+      const { username, password, host, port, database, dialect } =
+        this.dbConfig[this.environment];
+      this.connection = new Sequelize({
+        username,
+        password,
+        host,
+        port,
+        database,
+        dialect,
+        logging: this.isTestEnvironment ? false : console.log,
+      });
+  
+      // Check if we connected successfully
+      await this.connection.authenticate({ logging: false });
+  
+      if (!this.isTestEnvironment) {
+        console.log(
+          'Connection to the database has been established successfully'
+        );
+      }
+  
+      // Register the models
+      registerModels(this.connection);
+  
+      // Sync the models
+      await this.sync();
     }
-
-    async disconnect(){
-        await this.connect.close();
+  
+    async disconnect() {
+      await this.connection.close();
     }
-
-    async sync(){
-        await this.connect.sync({
-            logging: false,
-            force:this.isTestEnvirement,
-        });
-
-        if(!this.isTestEnvirement){
-            console.log("Connection synced successfuly");
-        }
+  
+    async sync() {
+      await this.connection.sync({
+        logging: false,
+        force: this.isTestEnvironment,
+      });
+  
+      if (!this.isTestEnvironment) {
+        console.log('Connection synced successfully');
+      }
     }
-}
+  }
